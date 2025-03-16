1f0f3b740713aeec43c8455eabd2ffe01f1ba4d3
Prabhu K H <25prabhu10@gmail.com>
2025-03-16 12:31:47 -0400

    chore: fix textlint script

---
+++ .textlintrc.cjs
@@ -0,0 +1,45 @@
+//rules:
+//  "@textlint-rule/no-unmatched-pair": true
+//
+//  common-misspellings: true
+//
+//  diacritics: true
+//
+//  spelling:
+//    language: "en-gb"
+//    skipPatterns: ["/\\d/g"]
+//    suggestCorrections: true
+//
+//  en-capitalization: true
+//
+//  terminology:
+//    defaultTerms: true
+//
+//  write-good:
+//    severity: "warning"
+//
+//filters:
+//  comments: true
+
+module.exports = {
+  rules: {
+    "@textlint-rule/no-unmatched-pair": true,
+    "common-misspellings": true,
+    diacritics: true,
+    //spelling: {
+    //  language: "en-gb",
+    //  skipPatterns: ["/\\d/g"],
+    //  suggestCorrections: true
+    //},
+    "en-capitalization": true,
+    terminology: {
+      defaultTerms: true
+    },
+    "write-good": {
+      severity: "warning"
+    }
+  },
+  filters: {
+    comments: true
+  }
+};
